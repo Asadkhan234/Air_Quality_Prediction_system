e6a4ef0ae9ab09c1754446dda4d4620e90b33195
Asadullah <ak5963905@gmail.com>
2026-08-01 20:09:38 +0500

    Use environment variable for API base URL

--- dashboard/src/api/analyticsCharts.js
+++ dashboard/src/api/analyticsCharts.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+
+
+const API = import.meta.env.VITE_API_URL;
 
 export const getAQIDistribution = async () => {
     const response = await axios.get(`${API}/analytics/aqi-distribution`);
