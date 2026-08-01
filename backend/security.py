7ebda51014001a74e1d14aeab6847bb67b14b7bc
Asadullah <ak5963905@gmail.com>
2026-08-01 20:46:21 +0500

    Replace passlib with direct bcrypt to fix hashing bug

--- backend/security.py
+++ backend/security.py
@@ -1,4 +1,4 @@
-from passlib.context import CryptContext
+import bcrypt
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
@@ -10,11 +10,6 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import User
 import os
-
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
@@ -29,13 +24,16 @@ oauth2_scheme = OAuth2PasswordBearer(
 # ================= PASSWORD =================
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(
+        password.encode("utf-8"),
+        bcrypt.gensalt()
+    ).decode("utf-8")
 
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(
-        plain_password,
-        hashed_password
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
     )
 
 
